fad58fd28950984ccbbcd751c8ba2c846273ea8e
Mikko Tuke Virtanen <mikko.virtanen@thl.fi>
2017-09-07 17:19:49 +0300

    plots to check

--- testing.R
+++ testing.R
@@ -8,3 +8,12 @@ rstan_package_skeleton("momoStan",code_files=list.files("R/",full=TRUE),stan_fil
 ###
 library("devtools")
 document()
+###
+load("data/fimomodata.RData")
+summary(fimomodata)
+library("ggplot2")
+ggplot(fimomodata,aes(x=date,y=n/pop,group=age,colour=age))+geom_line()
+ggplot(fimomodata,aes(x=date,y=TEMP,group=age,colour=age))+geom_line()
+ggplot(fimomodata,aes(x=date,y=InfA,group=age,colour=age))+geom_line()
+ggplot(fimomodata,aes(y=n/pop,x=TEMP,group=age,colour=age))+geom_point()+facet_wrap(~age,scales="free_y",ncol=1)
+ggplot(fimomodata,aes(y=n/pop,x=InfA,group=age,colour=age))+geom_point()+facet_wrap(~age,scales="free_y",ncol=1)
